7627aff5a7b424d3b87450efe8987cb0f3643cd4
lrondon prog <feliperondon123@hotmail.com>
2022-11-01 16:25:37 -0400

    Routes user, guesses

--- server/src/server.ts
+++ server/src/server.ts
@@ -24,6 +24,19 @@ async function bootstrap() {
         return { count }
     })
 
+    fastify.get('/users/count', async () => {
+        const count = await prisma.user.count()
+    
+        return { count }
+    })
+
+    fastify.get('/guesses/count', async () => {
+        const count = await prisma.guess.count()
+    
+        return { count }
+    })
+
+    
     fastify.post('/pools', async (request, reply) => {
 
         const createPoolBody = z.object({
