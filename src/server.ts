21f505eea7a26bd9dcd7aca114f4fad8c5e74fb1
lrondon-prog <feliperondon123@hotmail.com>
2022-10-31 23:42:56 -0300

    Pool count

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,9 @@
 import Fastify from "fastify";
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient({
+    log: ['query'],
+})
 
 async function bootstrap() {
     const fastify = Fastify({
@@ -6,12 +11,14 @@ async function bootstrap() {
     })
 
 
-    fastify.get('/pools/count', () => {
-        return { count: 0 }
+    fastify.get('/pools/count', async () => {
+        const count = await prisma.pool.count()
+    
+        return { count }
     })
 
     await fastify.listen({ port: 3333 })
 }
 
 
-bootstrap();
+bootstrap()
